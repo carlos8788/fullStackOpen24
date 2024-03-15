c2484ce922389607dc65b03510b72d2d05f7e79d
Luis <100473510+carlos8788@users.noreply.github.com>
2024-03-15 09:16:07 -0300

    ex 4.1 ready

---
+++ part4/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const {MONGODB_URI} = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const app = express()
+
+
+
+mongoose.connect(MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
+
