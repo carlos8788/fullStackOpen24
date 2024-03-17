4811301e8856a8da3b60da1227efe4994ae00ae5
Luis <100473510+carlos8788@users.noreply.github.com>
2024-03-17 06:24:58 -0300

    4.9 ready

--- part4/test/blog_api.test.js
+++ part4/test/blog_api.test.js
@@ -14,11 +14,20 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
+
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('Exist id property', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.status).toBe(200)
+  expect(response.body.length).toBeGreaterThan(0) 
+  expect(response.body[0].id).toBeDefined()
 })
 
 afterAll(() => {
