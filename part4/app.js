0eb518fda0196c1f595ab94f9716d97f161530cf
Luis <100473510+carlos8788@users.noreply.github.com>
2024-03-19 06:15:03 -0300

    exercice 4.15 ready

--- part4/app.js
+++ part4/app.js
@@ -5,6 +5,8 @@ require('express-async-errors')
 
 const {MONGODB_URI} = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const app = express()
 
 
@@ -14,6 +16,8 @@ mongoose.connect(MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 module.exports = app
