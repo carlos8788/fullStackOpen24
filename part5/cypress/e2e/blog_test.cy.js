6360ab79f7f391e412d612260414b2c8fe710456
Luis <100473510+carlos8788@users.noreply.github.com>
2024-03-31 16:48:29 -0300

    5.23 ready

--- part5/cypress/e2e/blog_test.cy.js
+++ part5/cypress/e2e/blog_test.cy.js
@@ -58,7 +58,7 @@ describe('Login other user', function () {
     cy.visit('http://localhost:5173');
   })
 
-  it('visits the app and logs in', function () {
+  it('view the blg and add like', function () {
 
     cy.get('input[name=username]').type('test2');
     cy.get('input[name=password]').type('123456');
@@ -74,4 +74,84 @@ describe('Login other user', function () {
 
   });
 
-});
+  describe('Login', function () {
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('123456')
+      cy.get('#loginBtn').click()
+      cy.contains('Wrong username or password')
+
+    })
+    it('succeeds with correct credentials', function () {
+      cy.visit('http://localhost:5173')
+      cy.get('#username').type('test_user')
+      cy.get('#password').type('123456')
+      cy.get('#loginBtn').click()
+      cy.contains('Log in')
+    })
+  })
+  describe('When logged delete blog', function () {
+    beforeEach(function () {
+      cy.visit('http://localhost:5173')
+      cy.get('#username').type('test_user')
+      cy.get('#password').type('123456')
+      cy.get('#loginBtn').click()
+      cy.contains('Log in')
+
+    })
+
+    it('A blog can be created', function () {
+      cy.get('#view').click()
+      cy.get('.danger').click()
+      
+      
+      
+      
+    })
+  })
+  describe('Create blogs and order DESC', function () {
+    beforeEach(function () {
+      
+      cy.visit('http://localhost:5173');
+      cy.get('#username').type('test_user');
+      cy.get('#password').type('123456');
+      cy.get('#loginBtn').click();
+      
+      let authToken;
+      cy.request('POST', 'http://localhost:3003/api/login', {
+        username: 'test_user',
+        password: '123456',
+      }).then((response) => {
+        
+        console.log(response, 'responseRESPONSE');
+        authToken = response.body.token;
+        
+        const blogs = [
+          { title: 'title1', author: 'author1', url: 'url1', likes: 1 },
+          { title: 'title2', author: 'author2', url: 'url2', likes: 2 },
+          { title: 'title3', author: 'author3', url: 'url3', likes: 3 }
+        ];
+
+        blogs.forEach(blog => {
+          cy.request({
+            method: 'POST',
+            url: 'http://localhost:3003/api/blogs',
+            headers: {
+              'Authorization': `Bearer ${authToken}`
+            },
+            body: blog
+          });
+        });
+      });
+      cy.visit('http://localhost:5173');
+      
+    });
+
+    it('blogs are created and ordered by likes DESC', function () {
+      
+      
+    });
+  });
+
+})
