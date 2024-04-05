e138eecee37cb74fbe1ed774e3df0adb2b372aba
Luis <100473510+carlos8788@users.noreply.github.com>
2024-04-05 08:21:01 -0300

    6.15 ready

--- part6/6c/src/App.jsx
+++ part6/6c/src/App.jsx
@@ -1,20 +1,31 @@
 
+import { useEffect } from 'react'
 import Anecdotes from './components/Anecdotes'
 import Filter from './components/Filter'
 import NewAnecdotes from './components/NewAnecdotes'
 import Notification from './components/Notification'
-
+import anecdoteService from './services/anecdotes'
+import { useDispatch} from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
-  
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchAnecdotes = async () => {
+      dispatch(setAnecdotes(await anecdoteService.getAll()))
+    };
+    fetchAnecdotes();
+  }, []);
   return (
     <div>
-      <Notification/>
+      <Notification />
       <h2>Anecdotes</h2>
-      <Filter/>
-      <Anecdotes/>
+      <Filter />
+      <Anecdotes />
       <h2>create new</h2>
-      <NewAnecdotes/>
+      <NewAnecdotes />
     </div>
   )
 }
