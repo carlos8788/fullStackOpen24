13f30d129d13d58cbfca4a7782bdc64cd19f4fff
Luis <100473510+carlos8788@users.noreply.github.com>
2024-04-16 07:15:00 -0300

    6d React Query

--- part7/7.10/front/src/redux/store.js
+++ part7/7.10/front/src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import notificationReducer from './notificationSlice'
 import blogsReducer from './blogSlice';
-// import userReducer from './userSlice';
 
 export const store = configureStore({
     reducer: {
         notification: notificationReducer,
         blogs: blogsReducer,
-
     },
 });
