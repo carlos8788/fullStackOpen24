e1f28fe8147fe03fb7f329b17805b09f5a6b93d2
Luis <100473510+carlos8788@users.noreply.github.com>
2024-04-21 08:01:10 -0300

    7.15 ready

---
+++ part7/7.10/front/src/pages/User.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useLocation, useNavigate, useParams } from "react-router-dom"
+import { fetchOneUser } from "../redux/usersSlice"
+
+const User = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const id = useParams().id
+    const user = useSelector(state => state.users.currentUser.entity)
+    useEffect(()=> {
+        dispatch(fetchOneUser(id))
+    }, [dispatch])
+    return (
+        <>
+            <h3>{user.name}</h3>
+            <h4>added blogs</h4>
+            {user.blogs.map( blog => <li key={blog.id}>{blog.title}</li>)}
+            <button onClick={() => navigate('/users')}>BACK</button>
+        </>
+    )
+}
+export default User
--- part7/7.10/front/src/redux/usersSlice.js
+++ part7/7.10/front/src/redux/usersSlice.js
@@ -1,32 +1,57 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { getAllUsers } from '../services/users';
+import { getAllUsers, getUser } from '../services/users';
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
-    const users = await getAllUsers()
-    return users
-})
+    const users = await getAllUsers();
+    return users;
+});
 
-export const usersSlice = createSlice({
-    name: 'users',
-    initialState: {
-        users: [],
-        status: 'idle',
+export const fetchOneUser = createAsyncThunk('users/fetchOneUser', async (id) => {
+    const user = await getUser(id);
+    // console.log(user)
+    return user;
+});
+
+const initialState = {
+    entities: [],
+    loading: false,
+    error: null,
+    currentUser: {
+        entity: null,
+        loading: false,
         error: null
-    },
+    }
+}
+
+
+const usersSlice = createSlice({
+    name: 'users',
+    initialState,
     reducers: {},
     extraReducers: (builder) => {
         builder
             .addCase(fetchUsers.pending, (state) => {
-                state.status = 'loading';
+                state.loading = true;
             })
             .addCase(fetchUsers.fulfilled, (state, action) => {
-                state.status = 'succeeded';
-                state.users = action.payload;
+                state.entities = action.payload;
+                state.loading = false;
             })
             .addCase(fetchUsers.rejected, (state, action) => {
-                state.status = 'failed';
                 state.error = action.error.message;
+                state.loading = false;
             })
+            .addCase(fetchOneUser.pending, (state) => {
+                state.currentUser.loading = true;
+            })
+            .addCase(fetchOneUser.fulfilled, (state, action) => {
+                state.currentUser.entity = action.payload;
+                state.currentUser.loading = false;
+            })
+            .addCase(fetchOneUser.rejected, (state, action) => {
+                state.currentUser.error = action.error.message;
+                state.currentUser.loading = false;
+            });
     }
 });
 
