8876761245b8f962294d088b6aefb6d149bb64c1
Luis <100473510+carlos8788@users.noreply.github.com>
2024-04-22 18:36:31 -0300

    material ui 7.21 ready

--- part7/7.10/front/src/pages/Blogs.jsx
+++ part7/7.10/front/src/pages/Blogs.jsx
@@ -1,79 +1,23 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
-import { addCommentBlog, getBlog, updateBlogs } from '../redux/blogSlice'
-import Button from '@mui/material/Button';
+import { getBlog } from '../redux/blogSlice'
+import Estructure from '../components/Estructure'
 
-const Estructure = ({ blog }) => {
-    const [likes, setLikes] = useState(blog.likes);
+const Blogs = () => {
+    const id = useParams().id;
     const dispatch = useDispatch();
+    const blog = useSelector(state => state.blogs.currentBlog);
 
     useEffect(() => {
-        setLikes(blog.likes);
-    }, [blog.likes]);
-
-    const handleClick = () => {
-        dispatch(updateBlogs(blog.id));
-        setLikes(prevLikes => prevLikes + 1);
-    };
+        dispatch(getBlog(id));
+    }, [dispatch, id]);
 
     return (
-        <>
-            <h2>{blog?.title}</h2>
-            <a href={blog?.url}>{blog?.url}</a>
-            <p>{likes} likes</p>
-            {/* <button onClick={handleClick}>like</button> */}
-            <Button onClick={handleClick} variant='contained' >Like</Button>
-            <p>added by {blog?.user.name}</p>
-
-            <h3>Comments</h3>
-            <AddComment />
-            {
-                blog?.comments.map((comment, i) => <li key={i}>{comment}</li>)
-            }
-        </>
+        <div>
+            {blog && <Estructure blog={blog} />}
+        </div>
     );
 };
 
-const AddComment = () => {
-
-    const dispatch = useDispatch();
-    const id = useParams().id
-    const formRef = useRef(null);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const data = Object.fromEntries(new FormData(e.target))
-        console.log(id, data.comment)
-        dispatch(addCommentBlog({ id, ...data }))
-        formRef.current.reset()
-    }
-
-    return (
-        <>
-            <form onSubmit={handleSubmit} ref={formRef}>
-                <input type="text" name='comment' />
-                <button type='submit'>add comment</button>
-            </form>
-        </>
-    )
-}
-
-const Blogs = () => {
-    const id = useParams().id
-    const dispatch = useDispatch()
-    const blog = useSelector(state => state.blogs.currentBlog)
-    useEffect(() => {
-        dispatch(getBlog(id))
-    }, [])
-    return (
-        <div>
-            {
-                blog && <Estructure blog={blog} />
-            }
-
-        </div>
-    )
-}
-
-export default Blogs
+export default Blogs;
--- part7/7.10/front/src/pages/User.jsx
+++ part7/7.10/front/src/pages/User.jsx
@@ -1,23 +1,49 @@
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { Link, useNavigate, useParams } from "react-router-dom"
-import { fetchOneUser } from "../redux/usersSlice"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { fetchOneUser } from "../redux/usersSlice";
+
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+
+const StyledContainer = styled("div")(({ theme }) => ({
+  marginTop: theme.spacing(4),
+}));
 
 const User = () => {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const id = useParams().id
-    const user = useSelector(state => state.users.currentUser.entity)
-    useEffect(()=> {
-        dispatch(fetchOneUser(id))
-    }, [dispatch])
-    return (
-        <>
-            <h3>{user?.name}</h3>
-            <h4>added blogs</h4>
-            {user?.blogs.map( blog => <li key={blog.id}><Link to={`/blog/${blog.id}`}>{blog.title}</Link></li>)}
-            <button onClick={() => navigate('/users')}>BACK</button>
-        </>
-    )
-}
-export default User
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const id = useParams().id;
+  const user = useSelector((state) => state.users.currentUser.entity);
+
+  useEffect(() => {
+    dispatch(fetchOneUser(id));
+  }, [dispatch]);
+
+  return (
+    <StyledContainer>
+      <Typography variant="h3" gutterBottom>
+        {user?.name}
+      </Typography>
+      <Typography variant="h4" gutterBottom>
+        Added Blogs
+      </Typography>
+      <List>
+        {user?.blogs.map((blog) => (
+          <ListItem key={blog.id} button component={Link} to={`/blog/${blog.id}`}>
+            <ListItemText primary={blog.title} />
+          </ListItem>
+        ))}
+      </List>
+      <Button variant="contained" onClick={() => navigate("/users")}>
+        Back
+      </Button>
+    </StyledContainer>
+  );
+};
+
+export default User;
